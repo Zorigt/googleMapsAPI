c96273bc669756b5aa9ef291d60effb307b68892
Zorigt <zorigt@gmail.com>
2013-11-23 09:37:12 -0800

    added scaffold zip

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GoogleMapsAPI::Application.routes.draw do
+  resources :zip_codes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
